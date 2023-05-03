de0bfb5a3e6194fdc17cf1c7ef80548a62a4d597
Coda Victor <vitmax.coda@gmail.com>
2023-05-03 10:01:13 +0300

    lab 5_2

--- Lab5/src/App.tsx
+++ Lab5/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
 import { useStore } from './LocalStore';
 import './App.css';
 
@@ -6,40 +6,102 @@ function MyComponent() {
     const store = useStore();
 
     const [localData, setLocalData] = useState<any>({ name: '', type: '', year: '' });
-
     const myData = store.myData;
 
-    const handleAddObject = (event: React.FormEvent<HTMLFormElement>) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [editingIndex, setEditingIndex] = useState(-1);
 
+    const handleAddObject = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         if (!localData.name || !localData.type || !localData.year) {
             alert('Complete all fields!');
             return;
         }
-        store.addObject(localData);
+        setIsLoading(true);
+        setTimeout(() => {
+            store.addObject(localData);
+            localStorage.setItem('myData', JSON.stringify(store.myData));
+            setIsLoading(false);
+        }, 2000);
         setLocalData({ name: '', type: '', year: '' });
     };
 
     const handleDeleteObject = (index: number) => {
         store.deleteObject(index);
         setLocalData({ name: '', type: '', year: '' });
+        localStorage.setItem('myData', JSON.stringify(store.myData));
     };
 
+    const handleUpdateObject = (index: number, newData: any) => {
+        store.updateObject(index, newData);
+        setIsLoading(true);
+        setTimeout(() => {
+            localStorage.setItem('myData', JSON.stringify(store.myData));
+            setIsLoading(false);
+        }, 2000);
+
+        const dataToUpdate = store.myData[index];
+        setLocalData({ name: dataToUpdate.name, address: dataToUpdate.address, phone: dataToUpdate.phone });
+        setIsEditing(false);
+        setEditingIndex(-1);
+    };
+
+    const handleEditObject = (index: number) => {
+        const dataToUpdate = store.myData[index];
+        setLocalData({ name: dataToUpdate.name, address: dataToUpdate.address, phone: dataToUpdate.phone });
+        setIsEditing(true);
+        setEditingIndex(index);
+        setLocalData({ name: '', type: '', year: '' });
+    };
+
+    const handleSaveObject = () => {
+        if (editingIndex >= 0) {
+            handleUpdateObject(editingIndex, localData);
+            setLocalData({ name: '', type: '', year: '' });
+        }
+    };
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        const data = localStorage.getItem('myData');
+        if (data) {
+            store.setMyData(JSON.parse(data));
+        } else {
+            localStorage.setItem('myData', JSON.stringify(store.myData));
+        }
+    }, [store]);
+
+    useEffect(() => {
+        if (!isLoading) {
+            const data = localStorage.getItem('myData');
+            if (data) {
+                store.setMyData(JSON.parse(data));
+            }
+        }
+    }, [isLoading, store]);
+
+    function cancelEdit(){
+        setIsEditing(false);
+        setLocalData({ name: '', type: '', year: '' });
+    }
 
     return (
         <div className="container">
             <div className='start_img'>
-                
                 <div className="card_cont">
                     {myData.map((data: any, index: number) => (
                         <div key={index} className="card">              
-                            <span className='second'>{data.name}</span>                           
+                            <div className='card_ins'>
+                                <span className='names'>Name: </span><span className='second'>{data.name}</span>
+                            </div>                          
                             <div className='card_ins'>
                                 <span className='names'>Type: </span><span className='second'>{data.type}</span>
                             </div>
                             <div className='card_ins'>
                                 <span className='names'>Year: </span><span className='second'>{data.year}</span>
                             </div>
+                            <button className="button2" onClick={() => handleEditObject(index)}>Edit</button>
                             <button className="button2" onClick={() => handleDeleteObject(index)}>Delete</button>
                         </div>
                     ))}
@@ -58,9 +120,49 @@ function MyComponent() {
                         <span className='inp_txt'>Year </span>  
                         <input className="input" type="text" value={localData.year} onChange={e => setLocalData({ ...localData, year: e.target.value })} />
                     </div>
-                    <button className="button" type="submit">Insert</button>
+                    <button className="button" type="submit">{isLoading ? 'Loading...' : 'Insert'}</button>
                 </form>
-            </div>     
+            </div> 
+            {isEditing && (
+                <div className="edit_cont">
+                    <div className="edit_main">
+                        <span className='title'>Edit</span>
+                        <form className="edit_form" onSubmit={handleSaveObject}>
+                            <div className='inp_cont_edit'>
+                                <span className='inp_txt'>Name edit  </span>  
+                                <input
+                                    className="input"
+                                    type="text"
+                                    value={localData.name}
+                                    onChange={(e) => setLocalData({ ...localData, name: e.target.value })}
+                                />
+                            </div>
+                            <div className='inp_cont_edit'>
+                                <span className='inp_txt'>Type edit  </span>
+                                <input
+                                    className="input"
+                                    type="text"
+                                    value={localData.type}
+                                    onChange={(e) => setLocalData({ ...localData, type: e.target.value })}
+                                />
+                            </div>
+                            <div className='inp_cont_edit'>
+                                <span className='inp_txt'>Year edit  </span>
+                                <input
+                                    className="input"
+                                    type="text"
+                                    value={localData.year}
+                                    onChange={(e) => setLocalData({ ...localData, year: e.target.value })}
+                                />
+                            </div>
+                            <div className="edit_cont_buttons">
+                                <button className="button" type="submit">Save</button>
+                                <button className="button" onClick={cancelEdit}>Cancel</button>
+                            </div>
+                        </form>   
+                    </div>
+                </div>
+            )}    
         </div>
     );
 }
--- Lab5/src/LocalStore.ts
+++ Lab5/src/LocalStore.ts
@@ -1,22 +1,58 @@
 import { createContext, useContext } from 'react';
 import { observable, action } from 'mobx';
 
+export class LocalStorageManager {
+    prefix: string;
+
+    constructor(prefix: string) {
+        this.prefix = prefix;
+    }
+
+    setItem(key: string, value: any) {
+        localStorage.setItem(`${this.prefix}-${key}`, JSON.stringify(value));
+    }
+
+    getItem(key: string) {
+        const value = localStorage.getItem(`${this.prefix}-${key}`);
+        return value ? JSON.parse(value) : null;
+    }
+}
+
 export class LocalStore {
+    storageManager: LocalStorageManager;
     @observable myData: any = [];
+
+    constructor(storageManager: LocalStorageManager) {
+        this.storageManager = storageManager;
+
+        const storedData = this.storageManager.getItem('myData');
+        if (storedData) {
+            this.myData = storedData;
+        } else {
+            this.storageManager.setItem('myData', []);
+        }
+    }
+    @action updateObject(index: number, newData: any) {
+        this.myData[index] = newData;
+        this.storageManager.setItem('myData', this.myData);
+    }
 
     @action setMyData(data: any) {
         this.myData = data;
+        this.storageManager.setItem('myData', data);
     }
 
     @action addObject(data: any) {
         this.myData.push(data);
+        this.storageManager.setItem('myData', this.myData);
     }
-
 
     @action deleteObject(index: number) {
         this.myData.splice(index, 1);
+        this.storageManager.setItem('myData', this.myData);
     }
 }
 
-const StoreContext = createContext(new LocalStore());
+const storageManager = new LocalStorageManager('DateLocale');
+const StoreContext = createContext(new LocalStore(storageManager));
 export const useStore = () => useContext(StoreContext);
